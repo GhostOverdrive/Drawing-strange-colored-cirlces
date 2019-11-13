0c873201f4aae3d69dfd62e95da67e7b713b98fd
eliot8alderson <v.stafeev@mail.ru>
2019-11-13 19:47:47 +0500

    coloring and drawing

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QWidget, QPushButton, QApplication, QMainWindow
 from PyQt5.QtGui import *
-from PyQt5 import uic
+from UI import Ui_MainWindow
 import sys
 import random
 
 
-class Window(QMainWindow):
+class Window(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.setGeometry(500, 500, 800, 600)
-        self.setWindowTitle('Yellow Circles')
+        self.setWindowTitle('Different Circles')
         self.place = 0, 0, 0, 0
         self.pushButton.clicked.connect(self.draw)
 
     def paintEvent(self, e):
         qp = QPainter()
         qp.begin(self)
-        qp.setBrush(QColor(255, 255, 0))
+        qp.setBrush(QColor(random.randint(1, 255), random.randint(1, 255),\
+                           random.randint(1, 255)))
         qp.drawEllipse(*self.place)
         qp.end()
 
