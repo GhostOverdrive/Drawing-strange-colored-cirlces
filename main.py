2c5da1687e2adacb97e4acf49a1a7dd529a89ac3
eliot8alderson <v.stafeev@mail.ru>
2019-11-13 19:44:27 +0500

    changed for ui files

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QWidget, QPushButton, QApplication, QMainWindow
 from PyQt5.QtGui import *
-from UI import Ui_MainWindow
+from PyQt5 import uic
 import sys
 import random
 
 
-class Window(QMainWindow, Ui_MainWindow):
+class Window(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setupUi(self)
+        uic.loadUi('UI.ui', self)
         self.setGeometry(500, 500, 800, 600)
-        self.setWindowTitle('Different Circles')
+        self.setWindowTitle('Yellow Circles')
         self.place = 0, 0, 0, 0
         self.pushButton.clicked.connect(self.draw)
 
     def paintEvent(self, e):
         qp = QPainter()
         qp.begin(self)
-        qp.setBrush(QColor(random.randint(1, 255), random.randint(1, 255),\
-                           random.randint(1, 255)))
+        qp.setBrush(QColor(255, 255, 0))
         qp.drawEllipse(*self.place)
         qp.end()
 
