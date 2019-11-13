df11636192a11b3f67d053df402ecff3f87229aa
eliot8alderson <v.stafeev@mail.ru>
2019-11-13 18:49:41 +0500

    ah, here we go

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 from PyQt5.QtWidgets import QWidget, QPushButton, QApplication, QMainWindow
 from PyQt5.QtGui import *
-from UI import Ui_MainWindow
 import random
 import sys
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(601, 371)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(10, 10, 133, 23))
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Draw strange yellow circle"))
 
 
 class Window(QMainWindow, Ui_MainWindow):
